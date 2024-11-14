60e16fbf979b425e209bce03ff9a0498a1d0ed5c
KwonNaRu <kwonnaru@kakao.com>
2024-11-14 18:25:50 +0900

    CategoryDTO 기본 생성자 추가

--- src/main/java/com/naru/backend/dto/CategoryDto.java
+++ src/main/java/com/naru/backend/dto/CategoryDto.java
@@ -6,8 +6,10 @@ import com.naru.backend.model.Category;
 import com.naru.backend.model.Post;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class CategoryDto {
 
     private Long id;
