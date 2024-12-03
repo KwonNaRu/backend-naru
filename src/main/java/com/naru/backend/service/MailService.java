c6857dd7ff57b16de7c1c855da1600f907568da4
KwonNaRu <kwonnaru@kakao.com>
2024-12-03 17:49:25 +0900

    메일 인증 Url 환경별로 개별 적용

--- src/main/java/com/naru/backend/service/MailService.java
+++ src/main/java/com/naru/backend/service/MailService.java
@@ -1,6 +1,7 @@
 package com.naru.backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,11 +12,16 @@ public class MailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${app.base-url}")
+    private String baseUrl;
+
     public void sendVerificationEmail(String to, String token) {
+        String verificationLink = baseUrl + "/auth/verify?token=" + token + "&email=" + to;
+
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject("Naru 블로그 회원가입 인증 메일");
-        message.setText("인증 링크: http://localhost:8080/auth/verify?token=" + token + "&email=" + to);
+        message.setText("인증 링크: " + verificationLink);
         mailSender.send(message);
     }
 }
