12f4df93191b90534f056225883293f5372c76b4
KwonNaRu <kwonnaru@kakao.com>
2024-12-07 15:01:05 +0900

    불필요한 Jwt 인터셉터 제거

--- src/main/java/com/naru/backend/config/WebSocketConfig.java
+++ src/main/java/com/naru/backend/config/WebSocketConfig.java
@@ -1,23 +1,14 @@
 package com.naru.backend.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-import com.naru.backend.interceptor.JwtChannelInterceptor;
-
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    private final JwtChannelInterceptor jwtChannelInterceptor;
-
-    public WebSocketConfig(JwtChannelInterceptor jwtChannelInterceptor) {
-        this.jwtChannelInterceptor = jwtChannelInterceptor;
-    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -31,11 +22,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app");
-    }
-
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(jwtChannelInterceptor);
     }
 
 }
